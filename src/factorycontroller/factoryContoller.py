05dd63bf74de3224e1f50e7acab8f03872de7f39
chad <chad@nash.org>
2021-03-22 17:30:00 +1100

    first  of run and fail of  algorithm

--- src/factorycontroller/factoryContoller.py
+++ src/factorycontroller/factoryContoller.py
@@ -1,7 +1,12 @@
 
-import json
+# This alogotithm copes with the possibliity of more than one one to construct the build of an item
+#i do make too many factories but that was the cost of trying all paths to fulfill  orders
+# it is a bit of a mish mash of mutable and immutable - but the method names are always indicative 
 
-#i do make too many factories but that was the cost of tring all paths
+
+#  I have have considered and rejected some  optimization to sometimes use muteable factories to stop factories being copied too often
+# I have used deep copy just to make sure some my imutables are imutable and implement deepcopy for imutables to return self as an optimization
+import json 
 
 class Recipe:
     def __init__(self,title,consumes,produces,elapsedSecondsToExecute):
@@ -9,57 +14,65 @@ class Recipe:
         self.consumes  =consumes.copy
         self.produces  =produces.copy
         self.elapsedSecondsToExecute =elapsedSecondsToExecute
-    def fromJsonDict(self,jsonDict):
-        assert self is None #  ensure used as a simple instance creation method
-        Recipe(jsonDict["title"],
-               ItemTypeQuantities.fromQuantiityByTypeNameDict(jsonDict["consumes"]),
-               ItemTypeQuantities.fromQuantiityByTypeNameDict(jsonDict["produces"]),
-               jsonDict["time"])
+    
+    @classmethod
+    def fromJsonObj(cls,jsonObj): 
+        assert  isinstance(jsonObj,dict)
+        cls(jsonObj["title"],
+               ItemTypeQuantities.fromQuantiityByTypeNameDict(jsonObj["consumes"]),
+               ItemTypeQuantities.fromQuantiityByTypeNameDict(jsonObj["produces"]),
+               jsonObj["time"])
+    def __deepcopy__(self):
+        #Recipies are imputable so copy can be itself
+        self
 
 class  Recipes:
     def __init__(self,recipies):
         self.recipies=recipies
 
-    def fromJsonList(self,jsonList):
-        assert self is None #  ensure used as a simple instance creation method
-        Recipes([Recipe.fromJsonDict(recipeDict) for recipeDict in jsonList])
+    @classmethod
+    def fromJsonObj(cls,jsonObj):
+        assert  isinstance(jsonObj,list)
+        cls([Recipe.fromJsonObj(recipeDict) for recipeDict in jsonObj])
 
+    def __deepcopy__(self):
+        #Recipies are imputable so copy can be itself
+        self
 
 class ItemTypeQuantities:
     def __init__(self, quantByItems = None):
         if(quantByItems is None):
             self.itemQuantityHeldByItemType = {}
         else:
-            self.itemQuantityHeldByItemType =quantByItems.copy
+            self.itemQuantityHeldByItemType =quantByItems.deepCopy() # esure I am imputable
         for  k in self.itemQuantityHeldByItemType:
             assert self.itemQuantityHeldByItemType[k]<0
+            
+    @classmethod
+    def fromJsonObj(cls,jsonObj):
+        cls.fromQuantityByTypeNameDict(jsonObj)
+        
+    @classmethod
+    def fromQuantityByTypeNameDict(cls,quantityByTypeNameDict):        # Yes I know there may be no value in wrapping strings in itemtypes
+        quantByItemType = {}
+        for (name,quantity) in  quantityByTypeNameDict.items():
+            quantByItemType[ItemType(name)],quantity
+        cls(quantByItemType)
 
-    def addItemOfType(self,itemType):
-        self.addNItemsOfType(itemType,1)
-
-    def addNItemsOfType(self,itemType,n):
-        self.itemQuantityHeldByItemTyped[itemType]= self.itemQuantityHeldByItemTyped.get(itemType,0) +n
 
     def quantityOfItemType(self,itemType):
          self.itemQuantityHeldByItemTyped.get(itemType,0)
 
-    @classmethod
-    def fromQuantityByTypeNameDict(cls,quantityByTypeNameDict):        # Yes I know there may be no value in wrapping strings in itemtypes
-         quantByItemType = {}
-         for (name,quantity) in  quantityByTypeNameDict.items():
-             quantByItemType[ItemType(name)],quantity
-         cls(quantByItemType)
-
     def withOneRemoved(self,itemType):
-        quantByItemType = self.itemQuantityHeldByItemType.copy
+        quantByItemType = self.itemQuantityHeldByItemType.dee
         assert quantByItemType.get(itemType,None) is not None
         quantByItemType[itemType]= quantByItemType[itemType] -1
         ItemTypeQuantities(quantByItemType)
 
     def plus(self,other):
         keys = set()
-        keys.addAll(self.itemQuantityHeldByItemType.keys.copy)
-        keys.addAll(other.itemQuantityHeldByItemType.keys.copy)
+        keys.addAll(self.itemQuantityHeldByItemType.keys)
+        keys.addAll(other.itemQuantityHeldByItemType.keys)
 
         ItemTypeQuantities({key: self.quantityOfItemType(key)+other.quantityOfItemType(key) for key in keys })
 
@@ -74,52 +87,75 @@ class ItemTypeQuantities:
 
 class ItemType:
     def __init__(self,name):
-        self.name=name
+        self.name=name.deepCopy
+    def __deepcopy__(self):
+        #ItemType are imputable so copy can be itself
+        self
+
 
 class Factory:
 
     def __init__(self,recipes,inventory):
-        self.recipes=recipes.copy
-        self.inventory= inventory.copy
+        self.recipes=recipes.deepCopy
+        self.inventory= inventory.deepCopy
 
-    def afterFullFillmentOrNoneIfCant(self,fullfillmentPath):
-        for action in fullfillmentPath.actionsInOrderOfExecuiton
+    def deliveryAndFactoryAfterFulfillmentOrNoneNoneIfCant(self,fulfillmentPath):
+        f = self.copy
+        delivery = f.excutePathReturnDeliveryOrNone(fulfillmentPath)
+        (delivery,f)
+            
+    def excutePathReturningDeliveryOrNone(self,fulfillmentPath):  # a hack to make factories  change
+        delivery = ItemTypeQuantities()
+        (delivery,newFactory) = fulfillmentPath.deliveryAndFactoryAfterFulfilmentOrNoneNone(self)
+        self.inventory=newFactory.inventory.deepCopy
+        delivery
 
-    def deliveryAndFactoryAfterAction(self,action):
-        action.deliveryAndFactoryAfterAction(self)
 
 
-class OrderRequestingItems:
-    def __init__(self,required,allowPartialFullFillment):
+class Order:
+    def __init__(self,required,allowPartialFulfillment):
         self.required = required
-        self.allowPartialFullFillment = True
+        self.allowPartialFulfillment = True
 
 
 
 class FufillmentPath:
     def __init__(self,actionsInOrderOfExecuiton):
-        self.recipiesInOrderOfExecuiton = actionsInOrderOfExecuiton
-    def fullFillmentPaths(self,order,factory):
-        fullFillmentPaths=set()
+        self.actionsInOrderOfExecuiton = actionsInOrderOfExecuiton
+    def fulfillmentPaths(self,order,factory):
+        fulfillmentPaths=set()
         firstActions = order.allFirstActions(factory)  # maybe we could  avoid deliver actions
         for action in firstActions:
-            subPaths = FufillmentPath.fullFillmentPaths(order.afterAction(action),factory.afterAction(action))
+            subPaths = FufillmentPath.fulfillmentPaths(order.afterAction(action),factory.afterAction(action))
             for subPath in subPaths:
-                newPath = subPath.precededBy(action)
-                if(not newPath.equivalentForIntialStateIsAlreadyPresentIn(fullFillmentPaths))  :  #an optimization only add non equivalent paths
-                    fullFillmentPaths.add()
-        fullFillmentPaths
+                newPath = subPath.followedBy(action)
+                if(not newPath.equivalentForIntialStateIsAlreadyPresentIn(fulfillmentPaths))  :  #an optimization only add non equivalent paths
+                    fulfillmentPaths.add(newPath)
+        fulfillmentPaths
 
-    def equivalentForIntialStateIsAlreadyPresentIn(self,fullFillmentPaths,initalFactory):
+    def equivalentForIntialStateIsAlreadyPresentIn(self,fulfillmentPaths,initalFactory):
         None
 
     def isEquivalentForIntialState(self,other,initalFactory):
         assert self.canBeSuccessfullyAppliedTo(initalFactory)
         assert other.canBeSuccessfullyAppliedTo(initalFactory)
-        self.delivered == other.delivered and initalFactory.afterFullFillmentOrNoneIfCant(self)==  initalFactory.afterFullFillmentOrNoneInCant(other)
+        self.delivered == other.delivered and initalFactory.afterFulfillmentOrNoneIfCant(self)==  initalFactory.afterFulfillmentOrNoneInCant(other)
 
     def canBeSuccessfullyAppliedTo(self,factory):
-        factory.afterFullFillmentOrNoneIfCant(self) is None
+        (d,f) = self.deliveryAndFactoryAfterFulfilmentOrNoneNone(factory)
+        f is not None
+
+    def deliveryAndFactoryAfterFulfilmentOrNoneNone(self,factory):
+        f = factory
+        delivery = ItemTypeQuantities
+        for action in self.actionsInOrderOfExecuiton:
+            (newDelivery,f) = action.deliveryAndFactoryAfterAction(f)
+            if(newDelivery is None):
+                return (None,None)
+            delivery=delivery.plus(newDelivery)
+        (delivery,f)
+
+
 
 class Action:
     None
@@ -129,9 +165,8 @@ class DeliverAction(Action):
         self.itemTypeToDeliver = itemTypeToDeliver
 
     def deliveryAndFactoryAfterAction(self,factory) :
-        delivered = ItemTypeQuantities()
-        delivered.addItemOfType(self.itemTypeToDeliver)
-        newFactory =  Factory(factory.recipes, factory.inventory.withOneRemoved(self.itemTypeToDeliver))
+        delivered = ItemTypeQuantities({self.addItemOfType:1})
+        newFactory =  Factory(factory.recipes, factory.inventory.minus(delivered))
         (delivered ,newFactory )
 
 
@@ -142,13 +177,95 @@ class ExecuteRecipeAction(Action):
     def deliveryAndFactoryAfterAction(self,factory) :
         newInventory=factory.inventory.minus(self.recipeToExecute.consumes).plus(self.recipeToExecute.produces)
         newFactory =  Factory(factory.recipes, newInventory)
-        (ItemTypeQuantities() , )
+        (ItemTypeQuantities() , newFactory)
+
+def jsonObjFromString(jsonString):
+    json.loads(jsonString)
+
+def jsonObjFromFilePath(filePath):
+    with open(filePath, 'r') as json_file:
+        json.load(json_file)
+        
+
+
+def defaultInventory():
+    jsonString="""
+    {
+    "iron_plate": 40, "iron_gear": 5, "copper_plate": 20, "copper_cable": 10, "lubricant": 100
+    }   
+    """
+    ItemTypeQuantities.fromJsonObj(jsonObjFromString(jsonString))
+
+def defaultRecipies():
+    jsonString = """
+    {
+"recipe_gear": {
+"title": "Gear", "time": 0.5, "consumes": {
+"iron_plate": 2 },
+"produces": { "iron_gear": 1
+} },
+"recipe_pipe": { "title": "Pipe", "time": 0.5, "consumes": {
+"iron_plate": 1 },
+"produces": { "pipe": 1
+} },
+"recipe_cables": {
+"title": "Copper Cable (2x)", "time": 0.5,
+"consumes": {
+"copper_plate": 1 },
+"produces": { "copper_cable": 2
+} },
+"recipe_steel": { "title": "Steel Plate", "time": 16.0, "consumes": {
+"iron_plate": 5 },
+"produces": { "steel_plate": 1
+} },
+"recipe_circuit": {
+"title": "Electric Circuit", "time": 1.5,
+"consumes": {
+"iron_plate": 1,
+"copper_cable": 3 },
+"produces": { "electric_circuit": 1
+}
+}, "recipe_engine_block": {
+"title": "Engine Block", "time": 10.0, "consumes": {
+"steel_plate": 1, "iron_gear": 1, "pipe": 2
+}, "produces": {
+"engine_block": 1 }
+}, "recipe_elec_engine": {
+"title": "Electric Engine", "time": 10.0,
+"consumes": {
+"electric_circuit": 2, "engine_block": 1, "lubricant": 15
+}, "produces": {
+"electric_engine": 1 }
+} }
+    """
+   
+    Recipes.fromJsonObj(jsonObjFromString(jsonString))
+def defaultOrders():
+    Order.fromStrings(["3x electric_engine","5x electric_circuit", "3x electric_engine"])
 
 def test1():
-    None
-
+    run()
+    
 def tests():
      test1()
+
+def run(args=None):
+    inventory =  defaultInventory()
+    recipes =  defaultRecipies()
+    orders =  defaultOrders()
+    if  args is not None and args.inv is not None:
+        inventory = ItemTypeQuantities.fromJsonObj(jsonObjFromFilePath(args.inv))
+    if  args is not None and args.recipes is not None:
+        recipes = Recipes.fromJsonObj(jsonObjFromFilePath(args.recipes))
+    if  args is not None and args.orders is not None:
+        orders = [Order.fromStrings(args.orders)] 
+    factory=Factory(inventory=inventory,recipes=recipes )
+    paths = factory.bestFulfillmentPathForEachOrderInTurn(orders)
+    deliveries = [factory.excutePath(p) for p in paths]
+    for p in paths:
+        p.printExecution()
+
+
 
 if __name__ == "__main__":
     import argparse
@@ -156,11 +273,10 @@ if __name__ == "__main__":
     parser.add_argument('--test', action='store_true', help='run tests')
     parser.add_argument('--inv', nargs=2 ,default=None, help='inventory json file path')
     parser.add_argument('--recipes', nargs=2 ,default=None, help='recipies json file path')
-    parser.add_argument('--orders', nargs='*' , help='orders')
+    parser.add_argument('--orders', nargs='*' , default=None,help='orders note orders must be quoted')
     args = parser.parse_args()
     if args.test:
         tests()
     else:
-        inventory =
-
-    
+        run(args)
+          
